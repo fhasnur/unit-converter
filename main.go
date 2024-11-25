7f9d511007eb85b9c8f63f8ac11502bd086000b6
fhasnur <fandi.hasnur10@gmail.com>
2024-11-25 22:45:01 +0700

    feat: integrate Mustache template engine

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/mustache/v2"
+)
 
 func main() {
-	app := fiber.New()
+	engine := mustache.New("./templates", ".mustache")
+
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		return ctx.Render("index", fiber.Map{
+			"Result": "",
+		})
+	})
+
+	app.Static("/static", "./static")
 
 	err := app.Listen(":3000")
 	if err != nil {
